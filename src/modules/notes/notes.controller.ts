d0eca337eb10ff71ff85eaf30a17e40dec065156
MVaquero93 <marcvaquero93@gmail.com>
2021-01-29 11:48:06 +0100

    Create notes

--- src/modules/notes/notes.controller.ts
+++ src/modules/notes/notes.controller.ts
@@ -1,5 +1,16 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { NotesService } from './notes.service';
+import { NoteDto } from './note.dto';
+import { Note } from './note.schema';
 
 @Controller('notes')
 export class NotesController {
+
+  constructor(private readonly notesService: NotesService) {}
+
+  @Post()
+  async create(@Body('notes') notes: NoteDto[]) {
+    const result = await this.notesService.create(notes);
+    return result
+  }
 }
--- src/modules/notes/notes.service.ts
+++ src/modules/notes/notes.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { Note, NoteDocument } from './note.schema';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class NotesService {
 
-  constructor() {}
+  constructor(@InjectModel(Note.name) private noteModel: Model<NoteDocument>) {}
+
+  async create(notes: Note[]) {
+    return this.noteModel.insertMany(notes);
+  }
 }
